7db5878dbadf5289e6f6a72bf0b82f85e99c0688
Boltlord1 <hasanzahiid2@gmail.com>
2023-03-14 20:27:08 +0500

    Fully polish the game (no flaws) (real)

--- javascript.js
+++ javascript.js
@@ -3,24 +3,25 @@ const board = document.querySelector('#board');
 function createBoard(rowCount, rowLength) {
     const rows = [];
     const length = [];
-    for (let i = 0; i < rowCount; i++) rows.push(document.createElement('div'));
-    for (i = 0; i < rowLength; i++) length.push([]);
-    let l = 0
-    function addArray(item) {
-        for (let j = 0; j < rowLength; j++) item.push(document.createElement('div'));
-        function addClasses(obj) {
-            obj.classList.add('pixel');
-            for (i = 0; i < rowCount; i++) rows[l].appendChild(obj);
-        }
-        item.forEach(addClasses);
-        l++;
-    }
+    let i = 0;
+    let j = 0;
+    for (i = 0; i < rowCount; i++) rows.push(document.createElement('div'));
+    for (i = 0; i < rowCount; i++) length.push([]);
     function appendRows(item) {
         item.classList.add('row');
-        board.appendChild(item);
+        board.appendChild(item)
+    }
+    function addPixels(item) {
+        function appendPixels(obj) {
+            obj.classList.add('pixel');
+            for (i = 0; i < rowLength; i++) rows[j].appendChild(obj);
+        }
+        for (i = 0; i < rowLength; i++) item.push(document.createElement('div'));
+        item.forEach(appendPixels);
+        j++
     }
     rows.forEach(appendRows);
-    length.forEach(addArray);
+    length.forEach(addPixels);
     function getRandomColor() {
         function getRandomNumber() {return Math.floor(Math.random() * 256);}
         let red = getRandomNumber();
@@ -30,7 +31,7 @@ function createBoard(rowCount, rowLength) {
     }
     const pixels = document.querySelectorAll('.pixel');
     pixels.forEach((pixel) => {
-        pixel.addEventListener('mousemove', () => pixel.style.backgroundColor = getRandomColor());
+        pixel.addEventListener('mouseenter', () => pixel.style.backgroundColor = getRandomColor());
     });
 } createBoard(16, 16);
 
@@ -43,8 +44,14 @@ function deleteBoard() {
 
 function reset() {
     deleteBoard();
-    let rowCount = prompt('Number of Rows? (max 100)', 16);
-    let rowLength = prompt('Pixels per Row? (max 100)', 16);
+    let rowCount = prompt('Number of Rows? (max 128)', 16);
+    while (rowCount > 128) {
+        rowCount = prompt('Please choose 128 or a lower number:', 16);
+    }
+    let rowLength = prompt('Pixels per Row? (max 128)', 16);
+    while (rowLength > 128) {
+        rowLength = prompt('Please choose 128 or a lower number:', 16)
+    }
     createBoard(rowCount, rowLength);
 }
 const button = document.querySelector('#button');
