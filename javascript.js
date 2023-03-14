dd4923d82c7827243582d82f8eb0d99d61be9d4c
Boltlord1 <hasanzahiid2@gmail.com>
2023-03-14 13:34:59 +0500

    Create drawing board

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board');
+const rows = [];
+const columns = [];
+
+for (let i = 0; i < 16; i++) {
+    rows.push(document.createElement('div'));
+    columns.push([]);
+}
+
+let l = 0
+function addArray(item) {
+    for (let j = 0; j < 16; j++) item.push(document.createElement('div'));
+    function addClasses(obj) {
+        obj.classList.add('pixel');
+        for (i = 0; i < 16; i++) rows[l].appendChild(obj);
+    }
+    item.forEach(addClasses);
+    l++;
+}
+
+columns.forEach(addArray)
+
+function appendRows(item) {
+    item.classList.add('row');
+    item.classList.add('pixel');
+    board.appendChild(item);
+}
+
+rows.forEach(appendRows)
+
+console.log(columns.length)
+console.log(rows.length)
+console.log(columns)
